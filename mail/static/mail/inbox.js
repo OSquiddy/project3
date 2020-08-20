5d90b5fa0c2e3b12d4dea274b8c3130dd07660d3
OSquiddy <siddiqui.omar21@gmail.com>
2020-08-20 23:53:24 +0400

    Final Version

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -39,11 +39,24 @@ function compose_email() {
     .then(result => {
       // Print result
       console.log(result);
-      load_mailbox('sent');
+      document.querySelector('#message').style.display = 'block';
+      if(result.message) {
+        document.querySelector('#message').className = 'alert text-center alert-success';
+        document.querySelector('#message').innerHTML = result.message;
+      }
+      else {
+        document.querySelector('#message').className = 'alert text-center alert-danger';
+        document.querySelector('#message').innerHTML = result.error;
+      }
+      // Change the page to 'Sent' after an email has been sent
+      if(result.message === 'Email sent successfully.')
+        load_mailbox('sent');
+      document.querySelector('#message').style.display = 'block';
     })
     .catch(error => {
-      console.log('Error: ', error)
+      console.log('Error: ', error);
     });
+    // Do not refresh the page and go to the default route on submitting
     return false;
   }
 }
@@ -54,6 +67,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#single-email-view').style.display = 'none';
+  document.querySelector('#message').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -62,17 +76,21 @@ function load_mailbox(mailbox) {
     fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails => {
+      if(emails.length === 0) 
+        document.querySelector('#emails-view').append('There are no emails to view at the moment.');
       // Print emails
       console.log(emails);
-
+      // Variable 'unread' will keep track of the unread emails
+      let unread = 0;
       // Do something else with emails.
       emails.forEach(email => {
+        //Increment unread emails
+        if(!email.read)
+          unread++;
+        // Create the div that will contain each individual email in the list displayed in the mailbox
         let newdiv = document.createElement('div');
         newdiv.data = `${email.id}`;
         newdiv.style.display = 'flex';
-        // newdiv.innerHTML = `<b>Subject: ${email.subject} | Sender: ${email.sender} | ${email.timestamp}</b>
-        // <br> 
-        // ${email.body}`;
         if(email.read) {
           newdiv.style.backgroundColor = '#eee';
           newdiv.innerHTML = `
@@ -97,10 +115,14 @@ function load_mailbox(mailbox) {
         newdiv.style.border = '1px solid lightgrey';
         newdiv.style.padding = '10px 20px';
         newdiv.style.marginBottom = '15px'; 
+        // Append each email to the main inbox div
         document.querySelector('#emails-view').append(newdiv) 
         
+        // Manually add a cursor style to the div, since it's not a button
         newdiv.onmouseover = () => newdiv.style.cursor = 'pointer';
         newdiv.onclick = () => load_mail(`${email.id}`, `${mailbox}`);
+        if(mailbox === 'inbox')
+          document.querySelector(`#${mailbox}`).innerHTML = `Inbox <b>${unread}</b>`;
       });
 
     })
@@ -121,23 +143,19 @@ function load_mail(id, mailbox) {
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
-    // document.querySelector('#single-email-view').innerHTML = `
-    // from: ${email.sender}
-    // <br> to: ${email.recipients}
-    // <br> date: ${email.timestamp}
-    // <br> subject: ${email.subject}
-    // <hr>
-    // ${email.body} `;
+    //Fill the fields of the HTML template created
     document.querySelector('#senderField').innerHTML = `${email.sender}`;
     document.querySelector('#receiversField').innerHTML = `${email.recipients}`;
     document.querySelector('#dateField').innerHTML = `${email.timestamp}`;
     document.querySelector('#subjectField').innerHTML = `${email.subject}`;
     document.querySelector('#emailBody').innerHTML = `${email.body}`;
+
+    // Mark the email as read, when it is opened
     toggle_read(email.id, true);
+
+    // Display the additional button row at the bottom only for inbox and archived mails.
     if(mailbox !== 'sent') {
       const buttonRow = document.querySelector('.buttonRow');
-      // buttonRow.className = 'buttonRow';
-      // document.querySelector('#single-email-view').append(buttonRow);
       buttonRow.innerHTML = `
       <button class="btn btn-primary" id="read">Mark Unread</buttton>
       <button class="btn btn-primary" id="archive">Archive</button>
@@ -185,6 +203,7 @@ function toggle_read(id, bool) {
   })
   .then(() => {
     if(!bool)
+      // Change the page to 'Inbox' after marking an email as unread
       load_mailbox('inbox')
   });
 }
@@ -197,6 +216,7 @@ function toggle_archive(id, bool) {
       archived : bool
     })
   })
+  // Change the page to 'Inbox' after marking the email as unread
   .then(() => load_mailbox('inbox'));
 }
 
@@ -214,7 +234,7 @@ function reply(email) {
   else {
     document.querySelector('#compose-subject').value = `${email.subject}`;
   }
-  document.querySelector('#compose-body').value = `On ${email.timestamp}, ${email.sender} wrote: \n\n\t${email.body} \n \n`;
+  document.querySelector('#compose-body').value = `\tOn ${email.timestamp}, ${email.sender} wrote: \n\t  ${email.body} \n \n`;
 
   // If submit button has been clicked
   document.querySelector('#compose-form').onsubmit = () => {
@@ -233,11 +253,24 @@ function reply(email) {
     .then(result => {
       // Print result
       console.log(result);
-      load_mailbox('sent');
+      document.querySelector('#message').style.display = 'block';
+      if(result.message) {
+        document.querySelector('#message').className = 'alert text-center alert-success';
+        document.querySelector('#message').innerHTML = result.message;
+      }
+      else {
+        document.querySelector('#message').className = 'alert text-center alert-danger';
+        document.querySelector('#message').innerHTML = result.error;
+      }
+      // Change the page to 'Sent' after an email has been sent
+      if(result.message === 'Email sent successfully.')
+        load_mailbox('sent');
+      document.querySelector('#message').style.display = 'block';
     })
     .catch(error => {
-      console.log('Error: ', error)
+      console.log('Error: ', error);
     });
+    // Do not refresh the page and go to the default route on submitting
     return false;
   }
 }
